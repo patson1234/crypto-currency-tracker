6b9a4735369eb4d8a5799fa9974c3d7d28323c25
patson1234 <pralhoon@gmail.com>
2022-02-03 17:12:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css';
+import Coin from './Coin'
+
+const ENDPOINT = 'http://127.0.0.1:5000'
+function App() {
+  // setInterval(() => {
+  //   getData()
+  // }, 10000);
+//  let ws = new WebSocket('wss://stram.binance.com:9443/ws/etheur@trade')
+//  ws.onmessage = (event) =>{
+//    let data = event.data.JSON()
+//    console.log(data);
+//  }
+  const getData = () =>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setcoins(res.data)
+      
+      
+    }).catch(error => console.error(error));
+    
+  }
+  const [coins, setcoins] = useState([])
+  const [search, setsearch] = useState('')
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+      setcoins(res.data)
+      
+    }).catch(error => console.error(error));
+    
+  }, [])
+
+  const handleChange = e =>{
+    setsearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.price_change_24h}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
